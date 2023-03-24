b73ed522274f2b04bc08db481a3759c848d428f4
marcos rios <max.rios92@gmail.com>
2023-03-24 01:32:29 -0300

    form and login

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import Nav from './components/Nav.jsx';
 import About from './components/About.jsx';
 import Detail from './components/Detail.jsx';
 import Login from './components/Login.jsx';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
 
 function App() {
    const [characters, setCharacters] = useState([])
+   const location = useLocation()
+   const navigate = useNavigate();
+   const [access, setAccess] = useState(false);
+   const EMAIL = 'ejemplo@gmail.com';
+   const PASSWORD = 'unaPassword';
+
+   function login(inputs) {
+      if (inputs.password === PASSWORD && inputs.email === EMAIL) {
+         setAccess(true);
+         navigate('/home');
+      }
+   };
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
 
    function onSearch(id) {
       axios(`https://rickandmortyapi.com/api/character/${id}`)
@@ -34,9 +50,11 @@ function App() {
    }
    return (
       <div className='App'>
-         <Nav onSearch={onSearch} />
+         {
+            location.pathname === "/" ? null : (<Nav onSearch={onSearch} />)
+         }
          <Routes>
-            <Route path='/' element={<Login />} ></Route>
+            <Route path='/' element={<Login login={login}/>} ></Route>
             <Route path='/home' element={<Cards characters={characters} onClose={onClose} />} />
             <Route path='/about' element={<About />} />
             <Route path='/details/:id' element={<Detail />} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Login() {
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+export default function Login({login}) {
+    const [inputs, setInputs] = React.useState({
+        email: "",
+        password: ""
+    })
+    const [errors, setErrors] = React.useState({
+        email: "",
+        password: ""
+    })
+    function validate(inputs) {
+        const errors = []
+        if (!inputs.email) {
+            errors.email = "Debe haber un email"
+        } else if (!inputs.password) {
+            errors.password = "Debe haber un password"
+        } else if (!regexEmail.test(inputs.email)) {
+            errors.email = 'Debe ser un e-mail valido' 
+        }
+        return errors;
+    }
+
+    function handleChange(e) {
+        setInputs({
+            ... inputs,
+            [e.target.name]:e.target.value,
+        })
+        setErrors(validate({
+            ... inputs,
+            [e.target.name]:e.target.value,
+        }))
+
+    }
+    function handleSubmit(e) {
+        e.prevent.default()
+        const aux = Object.keys(errors)
+        if(aux.length === 0) {
+            //envio
+            setInputs({
+                email: "",
+                password: ""
+            })
+            setErrors({
+                email: "",
+                password: ""
+            })
+            login(inputs)
+            return alert('ok')
+        } return alert('Falta data')
+
+    }
   return (
     <div>
-        <label>password</label>
-        <input></input>
-        <Link to='/home'>
-          <button>ingresar</button>
-        </Link>
+        <form onSubmit={handleSubmit}>
+            <label>E-mail:</label>
+            <input name='email' value={inputs.email} onChange={handleChange}></input>
+            <p className='danger'>{errors.email}</p>
+            <label>password</label>
+            <input name='password' value={inputs.password} onChange={handleChange}></input>
+            <p className='danger'>{errors.password}</p>
+            {
+                Object.keys(errors).length === 0 ? (<Link to='/home'><button type= 'submit'>ingresar</button></Link>) : null
+            }
+        </form>
     </div>
   )
 }
